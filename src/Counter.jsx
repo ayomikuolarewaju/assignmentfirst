81a6724c890fd2ef6b1a8dc0c88f46854ed3e097
ayomikuolarewaju <ayomiku@ymail.com>
2025-09-02 15:39:35 +0100

    counter

---
+++ src/Counter.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+    const decrement = () => {
+        if (count === 0) {
+           return window.alert("Count cannot be less than 0");;
+          
+        } 
+        setCount(count - 1);
+    }
+
+  return (
+    <div>
+
+        <div>Counter</div>
+        <h1>Count: {count}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button> 
+
+    </div>    
+  )
+}
+
+export default Counter

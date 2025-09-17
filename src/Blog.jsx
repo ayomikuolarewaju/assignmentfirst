611c6bfb2c925d4e9c8e6555a7c890c3b71ede7c
ayomikuolarewaju <ayomiku@ymail.com>
2025-09-17 16:00:50 +0100

    correction

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function Blog({blog}) {
-    
+function Blog({ blog }) {
   return (
     <div>
-        {blog.map((bl,index)=>(
-            <div key={index}>
-                <h2>{bl.title}</h2>
-                <p>{bl.body}</p>
-            </div>
-        ))}
+      {blog.map((bl, index) => (
+        <div key={index}>
+          <ul>
+            <p>{bl.title}</p>
+            <li>{bl.body}</li>
+          </ul>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import Blog from './Blog.jsx'
-
-
+import React, { useEffect, useState } from "react";
+import Blog from "./Blog.jsx";
 
 function Home() {
-const [blog, setBlog] = useState(null);
-const [isPending, setIsPending] = useState(true);
-const [empty, setEmpty] = useState("");
+  const [blog, setBlog] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [empty, setEmpty] = useState("");
 
-useEffect(() => {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(response => response.json())
-    .then(data => {
-        setBlog(data)
-        setIsPending(false);  
-        if(!data){
-            setEmpty("No blog available");
-            console.log(empty);
+      .then((response) => response.json())
+      .then((data) => {
+        setBlog(data);
+        setIsPending(false);
+        if (!data) {
+          setEmpty("No blog available");
         }
-    })
-    .catch(error => console.error('Error fetching blog:', error));
-    
-}, [empty]);                                                                                 
+      })
+      .catch((error) => console.error("Error fetching blog:", error));
+  }, [empty]);
 
   return (
     <div>
-        {isPending && <div>Loading...</div>}
-        {blog ? <Blog blog={blog}/> : <div><p>{empty}</p></div>}
+      {isPending && <div>Loading...</div>}
+      {blog ? (
+        <Blog blog={blog} />
+      ) : (
+        <div>
+          <p>{empty}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
